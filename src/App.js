262b514979439807d3aa8c00a3ecfba2137f2cd5
Adam Pearson <adam.pearson94@gmail.com>
2021-11-10 08:54:30 +0000

    First commit
    Short practice with dog fact api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import './App.css'
+import axios from 'axios';
+import Button from '@mui/material/Button';
 
 function App() {
+
+  const [facts, setFacts] = useState([]);
+
+
+  const pullFact = () => {
+    axios.get("https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1")
+    .then((response) => {
+      setFacts(response.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+  }
+
+  useEffect(() => {
+    console.log(facts);
+  }, [facts]);
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Button variant="contained" onClick={pullFact}>
+        Get Fact
+      </Button>
+      {facts.map((fact, index) => <h2 key={index}>{fact.fact}</h2>)}
     </div>
   );
 }
